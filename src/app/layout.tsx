61f395613d47a3806cb08f5069325ad4385612b0
Seohy <seohy@example.com>
2026-02-02 20:55:35 +0900

    feat: add Google AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,12 @@ export default function RootLayout({
             gtag('config', 'G-WHWQBZ6E8H');
           `}
                 </Script>
+                <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2947913248390883"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                />
             </body>
         </html>
     )
